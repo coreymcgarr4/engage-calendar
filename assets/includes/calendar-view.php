187a12f8fb5f124b0000d93f0f0a8fe940237ece
Corey McGarr <coreymcgarr4@gmail.com>
2018-01-03 17:01:05 -0700

    updated schedule, added assets

---
+++ assets/includes/calendar-view.php
@@ -0,0 +1,423 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<div id="header">ENGAGE</div>
+
+<table class="table" id="engage-table">
+  <thead>
+    <tr>
+      <th></th>
+      <th class="small-column">
+        <span class="long">Monday</span>
+        <span class="short">Mon</span>
+        <span class="day">2/5</span>
+      </th>
+      <th class="small-column">
+        <span class="long">Tuesday</span>
+        <span class="short">Tue</span>
+        <span class="day">2/6</span>
+      </th>
+      <th class="large-column">
+        <span class="long">Wednesday</span>
+        <span class="short">Wed</span>
+        <span class="day">2/7</span>
+      </th>
+      <th class="large-column">
+        <span class="long">Thursday</span>
+        <span class="short">Th</span>
+        <span class="day">2/8</span>
+      </th>
+      <th class="small-column">
+        <span class="long">Friday</span>
+        <span class="short">Fri</span>
+        <span class="day active">2/9</span>
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="hour" rowspan=""><span>7:30</span></td>
+      <td class="rp-blue" rowspan="10">Setup</td>
+      <td class="rp-yellow center-text half-hour" rowspan="2">
+        <a data-toggle="modal" data-target="#myModal">Breakfast</a>
+      </td>
+
+            <!--START MODAL-->
+            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                      <h4 class="modal-title green-text" id="myModalLabel">Breakfast</h4>
+                  </div>
+
+                  <div class="modal-body">
+                    <div class="row modal-descriptions">
+                      <div class="col-lg-4 col-md-4">Date</div>
+                      <div class="col-lg-8 col-md-8">February 6, 2018</div>
+                    </div>
+                    <div class="row modal-descriptions">
+                      <div class="col-lg-4 col-md-4">Time</div>
+                      <div class="col-lg-8 col-md-8">7:00 AM</div>
+                    </div>
+                    <div class="row modal-descriptions">
+                      <div class="col-lg-4 col-md-4">Location</div>
+                      <div class="col-lg-8 col-md-8">Mirage Conference Room</div>
+                    </div>
+                    <div class="row modal-descriptions">
+                      <div class="col-lg-4 col-md-4">Description</div>
+                      <div class="col-lg-8 col-md-8 modal-width">Learn how to eat a proper breakfast bla bla bla etc etc ytrht rehtre
+                                                                 htrehrdhgrdhgdhg dhgfdhfgdhfgdhsfg dshfgshfgetcytrhtrehtr ehtrehrdhgrdet
+                                                                 cytrhtrehtreht rehrdhgrdetcyt rhtrehtrehtrehrdh grdetcytrhtrehtrehtrehrdhgrdetcy trhtrehtrehtrehr dhgrdetcytrhtrehtrehtrehrdhgrd</div>
+                    </div>
+                  </div>
+
+                  <div class="modal-footer">
+                    <span class="modal-footer-text white-text">Tuesday @ 7:00 AM</span>
+                    <div title="Add to Calendar" class="addeventatc calendar-btn">
+                      Add to Calendar
+                      <span class="start">02/05/2018 07:00 AM</span>
+                      <span class="end">02/05/2018 12:00 PM</span>
+                      <span class="timezone">America/Los_Angeles</span>
+                      <span class="title">Setup</span>
+                      <span class="description">This is setup crew only</span>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+            <!--END MODAL-->
+
+
+      <td class="rp-yellow center-text" rowspan="3">Breakfast</td>
+      <td class="rp-yellow center-text" rowspan="3">Breakfast</td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>8:00</span></td>
+      <td class="bootcamps center-text" rowspan="4">
+          <div class="col-lg-6 col-md-6 col-sm-6 center-text">
+            <div class="center-text">Beginners Bootcamp</div>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-6 center-text">
+            <div class="center-text">Advanced Bootcamp</div>
+          </div>
+      </td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>8:30</span></td>
+      <td class="rp-red" rowspan="3">Opening Keynote</td>
+      <td class="rp-red" rowspan="3">Session: Customer Q & A Panel</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>9:00</span></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>9:30</span></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>10:00</span></td>
+      <td class="rp-yellow">Coffee Break</td>
+      <td class="rp-yellow">Coffee Break</td>
+      <td class="rp-yellow">Coffee Break</td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>10:30</span></td>
+      <td class="bootcamps" rowspan="3">
+        <div class="col-lg-6 col-md-6 col-sm-6">
+          <div>Beginners Bootcamp</div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+          <div>Advanced Bootcamp</div>
+        </div>
+      </td>
+      <td class="rp-red" rowspan="3">Session: Product Roadmap</td>
+      <td class="meet-experts" rowspan="3">
+        <div class="col-lg-4 col-md-4 col-sm-4">Meet the Experts</div>
+        <div class="col-lg-4 col-md-4 col-sm-4 bus-track">Business Track: Supplier Onboarding & Mgmt</div>
+        <div class="col-lg-4 col-md-4 col-sm-4">Tech Track: Form Builder</div>
+      </td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>11:00</span></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>11:30</span></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>12:00</span></td>
+      <td class="rp-dark-gray" rowspan="12">Arrivals / Registration</td>
+      <td class="rp-yellow center-text" rowspan="2">Lunch</td>
+      <td class="rp-yellow center-text" rowspan="2">Lunch</td>
+      <td class="rp-yellow center-text">Break</td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>12:30</span></td>
+      <td class="rp-red" rowspan="3">Closing Keynote, Awards, Prizes, and Lunch</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>1:00</span></td>
+      <td class="bootcamps" rowspan="4">
+        <div class="col-lg-6 col-md-6 col-sm-6">
+          <div>Beginners Bootcamp</div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+          <div>Advanced Bootcamp</div>
+        </div>
+      </td>
+      <td class="meet-experts" rowspan="3">
+        <div class="col-lg-4 col-md-4 col-sm-4">Meet the Experts</div>
+        <div class="col-lg-4 col-md-4 col-sm-4 bus-track">Business Track: Navigating & Mastering the Experience Econ</div>
+        <div class="col-lg-4 col-md-4 col-sm-4">Tech Track: Mobile Auditor</div>
+      </td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>1:30</span></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>2:00</span></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>2:30</span></td>
+      <td class="rp-yellow">Break</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>3:00</span></td>
+      <td class="rp-yellow">Coffee Break</td>
+      <td class="meet-experts" rowspan="3">
+        <div class="col-lg-4 col-md-4 col-sm-4">Meet the Experts</div>
+        <div class="col-lg-4 col-md-4 col-sm-4 bus-track">Business Track: Pro Svcs Best Practices</div>
+        <div class="col-lg-4 col-md-4 col-sm-4">
+          <a data-toggle="modal" data-target="#tt-managing-suppliers">Tech Track: Managing Suppliers</div></a>
+      </td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>3:30</span></td>
+      <td class="bootcamps" rowspan="3">
+        <div class="col-lg-6 col-md-6 col-sm-6">
+          <div>Beginners Bootcamp</div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+          <div>Advanced Bootcamp</div>
+        </div>
+      </td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>4:00</span></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>4:30</span></td>
+      <td class="rp-yellow">Coffee Break</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>5:00</span></td>
+      <td class="rp-yellow" rowspan="2">Break</td>
+      <td class="meet-experts" rowspan="3">
+        <div class="col-lg-4 col-md-4 col-sm-4">Meet the Experts</div>
+        <div class="col-lg-4 col-md-4 col-sm-4 bus-track">Business Track: PCA Management</div>
+        <div class="col-lg-4 col-md-4 col-sm-4">Tech Track: Supplier Onboarding</div>
+      </td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>5:30</span></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>6:00</span></td>
+      <td></td>
+      <td class="rp-dark-gray" rowspan="2">Welcome Reception</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>6:30</span></td>
+      <td></td>
+      <td class="rp-yellow" rowspan="2">Break</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>7:00</span></td>
+      <td class="rp-green" rowspan="4">CAE/Exec Customer/Prospect Dinners (Sales)</td>
+      <td class="rp-green" rowspan="4">CAE/Exec Customer/Prospect Dinners (Sales)</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>7:30</span></td>
+      <td class="rp-yellow" rowspan="3">Dinner</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>8:00</span></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <td class="hour" rowspan=""><span>8:30</span></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+  </tbody>
+</table>
+
+            <!--START MODAL-->
+            <div class="modal fade" id="tt-managing-suppliers" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                      <h4 class="modal-title green-text" id="myModalLabel">Managing Suppliers from Start to Finish - Supplier Quality Management Made Easy</h4>
+                  </div>
+
+                  <div class="modal-body">
+                    <div class="row modal-descriptions">
+                      <div class="col-lg-4 col-md-4 purple-text">Date</div>
+                      <div class="col-lg-8 col-md-8">February 7, 2018</div>
+                    </div>
+                    <div class="row modal-descriptions">
+                      <div class="col-lg-4 col-md-4 purple-text">Time</div>
+                      <div class="col-lg-8 col-md-8">3:00 PM</div>
+                    </div>
+                    <div class="row modal-descriptions">
+                      <div class="col-lg-4 col-md-4 purple-text">Location</div>
+                      <div class="col-lg-8 col-md-8">Mirage Conference Room</div>
+                    </div>
+                    <div class="row modal-descriptions">
+                      <div class="col-lg-4 col-md-4 purple-text">Description</div>
+                      <div class="col-lg-8 col-md-8 modal-width">From onboarding to supplier approval to managing supplier information, RizePoint has you covered. This technical breakout session is a complete introduction to all the RizePoint tools available today and the capabilities coming your way in the important area of supplier quality management.
+</div>
+                    </div>
+                  </div>
+
+                  <div class="modal-footer">
+                    <span class="modal-footer-text white-text">Wednesday @ 3:00 PM</span>
+                    <div title="Add to Calendar" class="addeventatc calendar-btn">
+                      Add to Calendar
+                      <span class="start">02/07/2018 03:00 PM</span>
+                      <span class="end">02/07/2018 04:30 PM</span>
+                      <span class="timezone">America/Los_Angeles</span>
+                      <span class="title">Tech Track: Managing Suppliers</span>
+                      <span class="description">This technical breakout session is a complete introduction to all the RizePoint tools available today and the capabilities coming your way in the important area of supplier quality management.</span>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+            <!--END MODAL-->
---
+++ assets/includes/modals.php
@@ -0,0 +1,44 @@
+<!--START MODAL-->
+<div class="modal fade" id="monday_12pm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title green-text" id="myModalLabel">Breakfast</h4>
+        </div>
+
+    <div class="modal-body">
+        <div class="row modal-descriptions">
+        <div class="col-lg-4 col-md-4">Date</div>
+            <div class="col-lg-8 col-md-8">February 6, 2018</div>
+        </div>
+        <div class="row modal-descriptions">
+            <div class="col-lg-4 col-md-4">Time</div>
+            <div class="col-lg-8 col-md-8">7:00 AM</div>
+        </div>
+        <div class="row modal-descriptions">
+            <div class="col-lg-4 col-md-4">Location</div>
+            <div class="col-lg-8 col-md-8">Mirage Conference Room</div>
+        </div>
+        <div class="row modal-descriptions">
+            <div class="col-lg-4 col-md-4">Description</div>
+            <div class="col-lg-8 col-md-8 modal-width">Write something about this event</div>
+        </div>
+    </div>
+
+    <div class="modal-footer">
+        <span class="modal-footer-text white-text">Tuesday @ 7:00 AM</span>
+        <div title="Add to Calendar" class="addeventatc calendar-btn">
+            Add to Calendar
+            <span class="start">02/05/2018 07:00 AM</span>
+            <span class="end">02/05/2018 12:00 PM</span>
+            <span class="timezone">America/Los_Angeles</span>
+            <span class="title">Setup</span>
+            <span class="description">This is setup crew only</span>
+        </div>
+    </div>
+
+    </div>
+    </div>
+</div>
+<!--END MODAL-->
