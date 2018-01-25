f345a394105d8038754fa193732833db4ce8ea7a
Corey McGarr <coreymcgarr4@gmail.com>
2018-01-25 11:50:50 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include 'assets/includes/header.php'; ?>
+	
+	<div id="cal">
+		<?php include 'assets/includes/calendar-view.php'; ?>
+	</div>
+	<div id="list">
+		<?php include 'assets/includes/list-view.php'; ?>  
+	</div>
+
+<?php include 'assets/includes/footer.php'; ?>
